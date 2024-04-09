47e9e56382994c7598c6e8b6b0bc7a8ef6bbf7b7
Joary13 <jolea13008@gmail.com>
2024-04-09 19:43:32 +0200

    global styles with styled element

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import styled from 'styled-components';
+import GlobalStyles from './styles/GlobalStyles';
+import Button from './ui/Button';
+import Input from './ui/Input';
 
 function App() {
   const H1 = styled.h1`
@@ -7,35 +10,20 @@ function App() {
     background-color: yellow;
   `;
 
-  const Button = styled.button`
-    font-size: 1.4rem;
-    padding: 1.2rem 1.6rem;
-    font-weight: 500;
-    border: none;
-    border-radius: 7px;
-    background-color: purple;
-    color: white;
-    cursor: pointer;
-    margin: 20px;
-  `;
-
-  const Input = styled.input`
-    border: 1px solid #ddd;
-    border-radius: 5px;
-    padding: 0.8rem 1.2rem;
-  `;
-
-  const StyledApp = styled.div`
+  const StyledApp = styled.main`
     background-color: orangered;
     padding: 20px;
   `;
   return (
-    <StyledApp>
-      <H1>The Wild Oasis</H1>
-      <Button onClick={() => alert('check in')}>Check in</Button>
-      <Button onClick={() => alert('check out')}>Check out</Button>
-      <Input type='number' placeholder='Number of guests' />
-    </StyledApp>
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The Wild Oasis</H1>
+        <Button onClick={() => alert('check in')}>Check in</Button>
+        <Button onClick={() => alert('check out')}>Check out</Button>
+        <Input type='number' placeholder='Number of guests' />
+      </StyledApp>
+    </>
   );
 }
 
